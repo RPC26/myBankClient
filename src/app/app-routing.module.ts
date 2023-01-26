042f9f209223305ac1ca8a50bde554cc16ccb449
Rubén Pérez <rpcausias@gmail.com>
2023-01-26 21:33:46 +0100

    Cuenta//TipoCuenta//Operacion//Tipooperacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CuentaEditAdminRoutedComponent } from './componente/application/cuenta/routed/admin/cuenta-edit-admin-routed/cuenta-edit-admin-routed.component';
+import { CuentaNewAdminRoutedComponent } from './componente/application/cuenta/routed/admin/cuenta-new-admin-routed/cuenta-new-admin-routed.component';
+import { CuentaPlistAdminRoutedComponent } from './componente/application/cuenta/routed/admin/cuenta-plist-admin-routed/cuenta-plist-admin-routed.component';
+import { CuentaRemoveAdminRoutedComponent } from './componente/application/cuenta/routed/admin/cuenta-remove-admin-routed/cuenta-remove-admin-routed.component';
+import { CuentaViewAdminRoutedComponent } from './componente/application/cuenta/routed/admin/cuenta-view-admin-routed/cuenta-view-admin-routed.component';
+import { OperacionNewAdminRoutedComponent } from './componente/application/operacion/routed/admin/operacion-new-admin-routed/operacion-new-admin-routed.component';
+import { OperacionPlistAdminRoutedComponent } from './componente/application/operacion/routed/admin/operacion-plist-admin-routed/operacion-plist-admin-routed.component';
+import { OperacionRemoveAdminRoutedComponent } from './componente/application/operacion/routed/admin/operacion-remove-admin-routed/operacion-remove-admin-routed.component';
+import { OperacionViewAdminRoutedComponent } from './componente/application/operacion/routed/admin/operacion-view-admin-routed/operacion-view-admin-routed.component';
 import { UsuarioEditAdminRoutedComponent } from './componente/application/usuario/routed/admin/usuario-edit-admin-routed/usuario-edit-admin-routed.component';
 import { UsuarioNewAdminRoutedComponent } from './componente/application/usuario/routed/admin/usuario-new-admin-routed/usuario-new-admin-routed.component';
 import { UsuarioPlistAdminRoutedComponent } from './componente/application/usuario/routed/admin/usuario-plist-admin-routed/usuario-plist-admin-routed.component';
@@ -11,13 +20,27 @@ import { LogoutComponent } from './componente/shared/routed/logout/logout.compon
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
+
   {path:"login", component: LoginComponent },
   { path: 'logout', component: LogoutComponent }, 
+
   {path: "plist/usuario", component: UsuarioPlistAdminRoutedComponent},
   {path: "admin/usuario/view/:id", component: UsuarioViewAdminRoutedComponent},
   {path: "admin/usuario/edit/:id", component: UsuarioEditAdminRoutedComponent },
   {path: "admin/usuario/remove/:id", component: UsuarioRemoveAdminRoutedComponent},
-  {path: "admin/usuario/new", component: UsuarioNewAdminRoutedComponent}
+  {path: "admin/usuario/new", component: UsuarioNewAdminRoutedComponent},
+
+  {path: "plist/cuenta", component: CuentaPlistAdminRoutedComponent},
+  {path: "admin/cuenta/view/:id", component: CuentaViewAdminRoutedComponent},
+  {path: "admin/cuenta/edit/:id", component: CuentaEditAdminRoutedComponent},
+  {path: "admin/cuenta/remove/:id", component: CuentaRemoveAdminRoutedComponent},
+  {path: "admin/cuenta/new", component: CuentaNewAdminRoutedComponent},
+  
+  {path: "plist/operacion", component: OperacionPlistAdminRoutedComponent},
+  {path: "admin/operacion/view/:id", component: OperacionViewAdminRoutedComponent},
+  {path: "admin/operacion/remove/:id", component: OperacionRemoveAdminRoutedComponent},
+  {path: "admin/operacion/new/", component: OperacionNewAdminRoutedComponent},
+  
 ];
 
 @NgModule({
