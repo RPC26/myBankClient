921ee3e7cab36dc79ac30476578e15dc71a3c5dd
Rubén Pérez <rpcausias@gmail.com>
2023-05-13 19:26:40 +0200

    arreglar env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { HttpHeaders } from "@angular/common/http";
+
 export const environment = {
   production: true
+};
+export const BASE_URL = "https://mybankserver-production.up.railway.app"
+
+export const HTTP_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json; charset=UTF-8',
+  }),
+  withCredentials: true
 };
